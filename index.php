068a172063928d3aacf94e5903954b20bd1d4ce9
andrianaivoa1 <halison.andrianaivoarivelo@etu.unilim.fr>
2025-10-20 01:08:11 +0200

    Modification du index.php

--- index.php
+++ index.php
@@ -1,1 +1,37 @@
-<!DOCTYPE html><html><head><meta charset="utf-8"><title>Timetitle> <link href="https://fonts.googleapis.comm/css?family=Aldrich" rel="stylesheet"> <link rel="stylesheet" href="asset/css/style.css"><script src="assets/js/script.js"></script></head><body><div id="wrapper"><div class="inside" id="wrapper"><div class="timer"><span id="hours">00</span><span id="minutes">00</span><span id="seconds">00</span></div><div class="controls"><button class="button pause"></button></div></div></div><body></html>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Timer</title>
+
+    <!-- Police Google corrigée -->
+    <link href="https://fonts.googleapis.com/css?family=Aldrich" rel="stylesheet">
+
+    <!-- Correction du chemin du fichier CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- Chargement du script JavaScript -->
+    <script src="assets/js/script.js" defer></script>
+</head>
+
+<body>
+    <!-- Conteneur principal -->
+    <div id="wrapper">
+        <!-- Élément intérieur qui contient le timer -->
+        <div class="inside">
+            
+            <!-- Bloc affichant l’heure -->
+            <div class="timer">
+                <span id="hours">00</span>
+                <span id="minutes">00</span>
+                <span id="seconds">00</span>
+            </div>
+
+            <!-- Bouton pour pause/play -->
+            <div class="controls">
+                <button class="button pause"></button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
