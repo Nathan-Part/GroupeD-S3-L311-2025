b0d9ff07e4011e89f2161b325b9e7ba41ae417a6
Yamine Daroueche <yamine.daroueche@etu.unilim.fr>
2025-10-20 02:34:32 +0200

    fix: modifs fichier script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,55 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-    let is_run    = "true";
+    let is_run = "true";
 
-    init();
+    // Formatage du timer (exemple, '9:7' devient '09:07')
+    function adjustTimer(timer){
+        (timer < 10 ? '0'+timer : timer);
+    }
+
+    function randomHexColor(x, y, z){
+        return "rgb(" 
+            + Math.floor(x/100 * 256) + "," 
+            + Math.floor(y/100 * 256) + ","
+            + Math.floor(z/100 * 256) + 
+        ")";
+    }
 
     function init(){
-       document.querySelector('.button').addEventListener('clic', (event) => 
-            is_run = !is_run
-            ((' ' + document.querySelector('.button').className + ' ').indexOf('pause') > -1) ? dcument.querySelector('.button').setAttribute('class', document.querySelector('.button').getAttribute('class').replace(' pause', '')) : document.querySelector('.button').setAttribute('class', document.querySelector('.button').getAttribute('class')+' pause');
-        })
+        // Cliquer sur le bouton change son etat (de 'play' a 'pause' et vice-versa)
+        document.querySelector('.button').addEventListener('clic', (event) => {
+           is_run = !is_run;
+           ((' ' + document.querySelector('.button').className + ' ').indexOf('pause') > -1) ?
+               document.querySelector('.button')
+               .setAttribute(
+                   'class',
+                   document.querySelector('.button').getAttribute('class').replace(' pause', '')
+               )
+               :
+               document.querySelector('.button')
+               .setAttribute(
+                   'class',
+                   document.querySelector('.button').getAttribute('class')+' pause'
+               );
+       })
+       console.log(is_run);
 
-        setTimeInterval(function(){
+        // Si le chrono est en marche, mise a jour de l'affichage 
+        // et changement de la couleur de fond chaque seconde 
+        setInterval(function(){
             if(is_run){
                 let oDate = new Date();
                 document.querySelector('#hours').innerHTML   =  adjustTimer(oDate.getHours());
                 document.querySelector('#minutes').innerHTML =  adjustTimer(oDate.getMinutes());
                 document.querySelector('#seconds').innerHTML =  adjustTimer( oDate.getSeconds());
 
-                document.querySelector('body').style.background = randomHexColor(document.querySelector('#hours').innerHTML, document.querySelector('#minutes').innerHTML, document.querySelector('#seconds').innerHTML);
-                
+                document.querySelector('body').style.background = randomHexColor(
+                    document.querySelector('#hours').innerHTML,
+                    document.querySelector('#minutes').innerHTML,
+                    document.querySelector('#seconds').innerHTML
+                );
             }
-        }, 1000;
+        }, 1000)
     }
 
-    function adjustTimer(timer){
-        (timer < 10 ? '0'+timer : timer);
-    }
-
-    function randomHexColor(x, y){
-        return "rgb(" + Math.floor(x/100 * 256) + "," + Math.floor(y/100 * 256) + "," + Math.floor(z/100 * 256) + ")";
-    }
+    init();
 });
